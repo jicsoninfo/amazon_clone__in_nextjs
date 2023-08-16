294788d1f275b1b4cf489f2782f966de7c6085df
jicsoninfo <jicsoninfo@gmail.com>
2023-08-16 18:41:44 +0530

    project setup

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import logo from "@/images/logo.png";
+import { SlLocationPin } from "react-icons/sl";
+
+const Header = () => {
+   return(
+    <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50" >
+        {/* <h1>Header</h1> */}
+        <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 md1:gap-3 px-4">
+           {/* logo */}
+           <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center h-[70%]">
+            <Image className="w-28 object-cover mt-1" src={logo} alt="logoimg" />
+           </div>
+            {/* <p>Hello</p> */}
+
+            {/* delivery */}
+            <div className="px-2 border border-transparent hover:border-white curson-pointer duration-300 items-center justify-center h-[70%] hidden xl:inline-flex gap-1">
+                <SlLocationPin />
+                <div className="text-xs">
+                    <p>Deliver to</p>
+                    <p className="text-white font-bold uppercase">USA</p>
+                </div>
+            </div>
+            {/* serchbar */}
+            {/* signin */}
+            {/* favorite */}
+            {/* cart */}
+
+        </div>
+    </div>
+   ) 
+    
+}
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss'
 
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,10 +9,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+      screens:{
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
+      colors :{
+        amazon_blue: "#131921",
+      },
+      fontFamily: {
+        bodyFont: ["Poppins", "sans-serif", "Roboto"],
       },
     },
   },
